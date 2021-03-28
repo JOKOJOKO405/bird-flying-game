a0cea184aa0ff7c52bef0454d8e93e7b5fc67924
JOKOJOKO405 <biz.kj405@gmail.com>
2021-03-28 16:57:07 +0900

    add キーイベントで移動

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,13 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 // 画像設定
-const image = new Image()
-image.src = './assets/img/128.png'
+const imgBird = new Image()
+imgBird.src = './assets/img/128.png'
 // 画面設定
 const canvasH = 500
-const canvasW = 792
+const canvasW = 800
 
-image.onload = () => {
-  ctx.drawImage(image, 0, 0, 128, 128, 0, 0, 128, 128)
-}
-
+// キャラクター格納配列
 const gameObjs = []
 
 class GameObject {
@@ -22,7 +19,7 @@ class GameObject {
     this.centerY = this.y + this.height / 2
     this.width = width
     this.height = height
-    this.column = 2
+    this.column = 0
     this.row = 0
     gameObjs.push(this)
   }
@@ -36,7 +33,17 @@ class GameObject {
     this.centerY = this.y + this.height / 2
   }
   draw(image) {
-    ctx.drawImage(image, this.column * 128, this.row * 128, 128, 128, this.x, this.y, this.width, this.height)
+    ctx.drawImage(
+      image,
+      this.column * this.width,
+      this.row * this.height,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    )
   }
 }
 
@@ -45,28 +52,57 @@ class Bird extends GameObject {
     super(x, y, width, height)
     this.image = image
     this.frameCount = 0
-    this.jump = true
+    this.jump = false
   }
-  update() {
-    this.frameCount++;
-    if(this.jump) {
-      if(this.frameCount % 12 === 0 && this.column === 3){
-        this.column = 2
-      }else if(this.frameCount % 12 === 0 && this.column === 2){
-        this.column = 3
-      }
-    }else{
-      if(this.frameCount % 12 === 0 && this.column === 0){
+  update(keyEvent) {
+    // 毎フレームカウントする
+    this.frameCount++
+    // スピード
+    // TODO コンストラクタに入れる？
+    let speed = 10
+    if (!keyEvent) {
+      if (this.frameCount % 20 === 0 && this.column === 0) {
         this.column = 1
-      }else if(this.frameCount % 12 === 0 && this.column === 1){
+      } else if (this.frameCount % 20 === 0 && this.column === 1) {
         this.column = 0
       }
+    } else {
+      if (keyEvent === 'ArrowRight' && this.x < canvasW - 128) {
+        this.x += speed
+      } else if (keyEvent === 'ArrowLeft' && this.x > 0) {
+        this.flipDraw(this.image)
+        this.x -= speed
+      } else if (keyEvent === 'ArrowUp' && this.y < canvasH) {
+        this.column = 2
+        if (this.frameCount % 20 === 0 && this.column === 3) {
+          this.column = 2
+        } else if (this.frameCount % 20 === 0 && this.column === 2) {
+          this.column = 3
+        }
+        this.y -= speed
+      }
     }
+    // if(this.jump) {
+    //   if(this.frameCount % 12 === 0 && this.column === 3){
+    //     this.column = 2
+    //   }else if(this.frameCount % 12 === 0 && this.column === 2){
+    //     this.column = 3
+    //   }
+    // }else{
+    //   if(this.frameCount % 20 === 0 && this.column === 0){
+    //     this.column = 1
+    //   }else if(this.frameCount % 20 === 0 && this.column === 1){
+    //     this.column = 0
+    //   }
+    // }
     this.draw(this.image)
   }
 }
 
-let bird = new Bird(image, 0, 0, 128, 128)
+let bird = new Bird(imgBird, 0, 500 - 128, 128, 128)
+imgBird.onload = () => {
+  ctx.drawImage(imgBird, 0, 0, 128, 128, 0, 0, 128, 128)
+}
 
 function mainLoop() {
   let loopId = window.requestAnimationFrame(mainLoop)
@@ -74,5 +110,8 @@ function mainLoop() {
   gameObjs.forEach((gameObj) => {
     gameObj.update()
   })
+  window.onkeydown = (event) => {
+    bird.update(event.code)
+  }
 }
-requestAnimationFrame(mainLoop);
+requestAnimationFrame(mainLoop)
