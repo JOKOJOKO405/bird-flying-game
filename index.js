e5766f4ae232151645dfe29e9abfabc6fb7310d5
JOKOJOKO405 <biz.kj405@gmail.com>
2021-05-14 21:21:49 +0900

    add シングルクオート

--- index.js
+++ index.js
@@ -6,43 +6,41 @@ const client = new Client({
 })
 
 const express = require('express')
-var path = require('path');
+var path = require('path')
 const app = express()
 const port = process.env.PORT || 5502
 
-const bodyParser = require("body-parser");
-const router = express.Router();
+const bodyParser = require('body-parser')
+const router = express.Router()
 
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // index.htmlを返す処理
-router.get('/',(req,res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-router.get('/game',(req,res) => {
-  res.sendFile(path.join(__dirname+'/game.html'));
-});
-router.get('/score',(req,res) => {
-  res.sendFile(path.join(__dirname+'/score.html'));
-});
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+router.get('/game', (req, res) => {
+  res.sendFile(path.join(__dirname + '/game.html'))
+})
+router.get('/score', (req, res) => {
+  res.sendFile(path.join(__dirname + '/score.html'))
+})
 
 // データ挿入
 app.post('/post_score', async (req, res) => {
   try {
-    const users = await client.query(
-      `SELECT username, score FROM account`
-    )
+    const users = await client.query(`SELECT username, score FROM account`)
     // 同じユーザーがいるか
-    const isSameUser = users.rows.some(row => {
+    const isSameUser = users.rows.some((row) => {
       return row.username === req.body.name
     })
-    if(!isSameUser){
+    if (!isSameUser) {
       await client.query(
         `INSERT INTO account (username, score, created_on, last_login) VALUES ( '${req.body.name}', ${req.body.score}, current_timestamp, current_timestamp)`
       )
-    }else{
+    } else {
       await client.query(
         `UPDATE account SET "score"=${req.body.score}, "last_login"=current_timestamp WHERE "username"='${req.body.name}'`
       )
@@ -50,29 +48,25 @@ app.post('/post_score', async (req, res) => {
   } catch (err) {
     console.error(err)
   }
-  res.send("Received POST Data!");
+  res.send('Received POST Data!')
 })
 
 // データ呼び出し
 app.get('/get_score', async (req, res) => {
-  try{
+  try {
     const data = await client.query(
       `SELECT username, score FROM account ORDER BY score DESC LIMIT 5`
-      )
-      console.log(data)
-      res.send(data); 
-  }catch(e){
+    )
+    console.log(data)
+    res.send(data)
+  } catch (e) {
     console.error(e)
   }
 })
 
-
 app.use(express.static('public'))
-app.use('/', router);
-app.use('/game.html', router);
-app.use('/score.html', router);
+app.use('/', router)
+app.use('/game.html', router)
+app.use('/score.html', router)
 
 app.listen(port)
-
-
-
